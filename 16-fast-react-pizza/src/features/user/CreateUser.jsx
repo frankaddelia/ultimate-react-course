c885c6f56b95ed1911580ef021bb21a88799b809
Frank <faddelia@gmail.com>
2024-12-29 14:52:41 -0600

    Use userSlice for username. Add cartReducer

--- 16-fast-react-pizza/src/features/user/CreateUser.jsx
+++ 16-fast-react-pizza/src/features/user/CreateUser.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
 
+  const dispatch = useDispatch();
+
+  const naviate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+
+    dispatch(updateName(username));
+    naviate('/order/new');
   }
 
   return (
